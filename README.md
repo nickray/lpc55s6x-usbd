387bcbeb3ec300fbca0dc249bed3598ad0dd37da
Nicolas Stalder <n@stalder.io>
2019-08-19 01:17:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# `lpc55s6x-usbd`
+
+[usb-device](https://github.com/mvirkkunen/usb-device) implementation
+for NXP LPC55S6x microcontrollers.
+
+This repository is based on the [stm32-rs/stm32-usbd](https://github.com/stm32-rs/stm32-usbd) crate.
+
+## Supported microcontrollers
+
+* `LPC55S66`
+* `LPC55S69`
+
+## License
+Apache-2.0 or MIT.
---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+//! USB peripheral driver for NXP LPC55S6x microcontrollers.
+//!
+//! Derivative of the `stm32-usbd` crate, which merged previous
+//! implementations for several STM32 microcontrollers.
+
+#![no_std]
+
+// mod endpoint;
+// mod endpoint_memory;
+
+// mod target;
+
+// /// USB peripheral driver.
+// pub mod bus;
+
+// pub use crate::bus::UsbBus;
+// pub use crate::target::usb_pins::UsbPinsType;
+// pub type UsbBusType = UsbBus<UsbPinsType>;
+
+// mod pac;
